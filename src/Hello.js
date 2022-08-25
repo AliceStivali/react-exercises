88673d6ef6d78d20fa5e6549c94b0a135b9370c2
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-25 18:57:59 +0200

    correction

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,15 @@
 import React from "react"
 
-class Hello extends React.Component {
+export class Hello extends React.Component {
     render() {
         return <h1>Hello, World!</h1>
     }
 }
 
-class App extends React.Component {
+export class App extends React.Component {
     render() {
-        return <div>{Hello}</div>
+        return <div>
+            <Hello />
+            </div>
     }
 }
