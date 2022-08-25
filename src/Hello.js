0762747d472501b2649fdec2e7bdab86ebfd8137
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-25 18:40:19 +0200

    components-03

--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,13 @@
+import React from "react"
+
 class Hello extends React.Component {
     render() {
         return <h1>Hello, World!</h1>
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <div>{Hello}</div>
     }
 }
