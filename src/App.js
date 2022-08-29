789a22740b5d4c817ed71fda8c209838082a44bd
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-29 16:22:13 +0200

    state-01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Counter } from "./Counter"
 
 export class App extends React.Component {
     render() {
         return <div>
             <Welcome name={<strong>Alice</strong>} age={30}/>
+            <Counter />
         </div>
     }
 }
