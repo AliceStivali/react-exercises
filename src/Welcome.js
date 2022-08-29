8581875c7d29cb5905d7cca41826732402bd559f
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-29 12:22:48 +0200

    conditional-rendering-05

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,9 +4,9 @@ export class Welcome extends React.Component {
     render() {
         return <div>
             <h2>Hello, {this.props.name}!</h2>
-            {this.props.age > 18 && <Age />}
-            {this.props.age < 65 && <Age />}
-            {this.props.name === 'John' && <Age />}
+            {this.props.age > 18 
+            ? <Age />
+            : <p>You are very young!</p>}
             </div>
     }
 }
