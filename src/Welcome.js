87b4ccea14d25c4e581febc35c2f645355525952
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-30 12:36:38 +0200

    correction

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Age } from "./Age"
 
 export class Welcome extends React.Component {
     render() {
         return <div>
             <h2>Hello, {this.props.name}!</h2>
             {this.props.age > 18 
-            ? <Age />
+            ? <Age age={this.props.age}/>
             : <p>You are very young!</p>}
             </div>
     }
