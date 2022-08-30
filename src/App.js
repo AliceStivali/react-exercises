0a50c56283e1fe7e947d33977986a3989968e5e4
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-08-30 12:53:43 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { Welcome } from "./Welcome"
 import { Counter } from "./Counter"
 
 export class App extends React.Component {
     render() {
         return <div>
             <Welcome name={<strong>Alice</strong>} age={30}/>
-            <Counter />
+            <Counter initialValue={0} incrementedBy={1} timeout={1000}/>
         </div>
     }
 }
