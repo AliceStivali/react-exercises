899a38b2487ecc8e4a7d1b5caebd58d6ca0e1e4b
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-06 10:30:35 +0200

    forms-03

--- src/Login.js
+++ src/Login.js
@@ -4,35 +4,46 @@ export class Login extends React.Component {
     state = {
         username: '',
         password: '',
-        remember: false
+        remember: false,
+        disabled: true,
     }
     handleInputChange = (event) => {
         const value = event.target.value
         const name = event.target.name
-
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleCheckboxChange = (event) => {
-        const name = event.target.name
+        const type = event.target.type
         const checked = event.target.checked
 
         this.setState({
-            [name]: checked
+            [name]: type === 'checkbox' ? checked : value
         })
     }
 
+    onLogin = () => {
+        this.setState({
+            button: this.username === '' || this.password === '' ? true : false
+        })
+    }
+
+    setDisabledButton = () => {
+        if(this.state.username && this.state.password && this.state.disabled) {
+            this.setState({disabled: false})
+        } else if((!this.state.username || !this.state.password) && !this.state.disabled) {
+            this.setState({disabled: true})
+        }
+    }
+
+
     render() {
+        this.setDisabledButton()
         return <div>
             <h2>Username</h2>
             <input name="username" value={this.state.username} onChange={this.handleInputChange} />
             <h2>Password</h2>
             <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
             <span>Remember
-            <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleCheckboxChange} />
+            <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange} />
             </span>
+            <div><button name="login" disabled={this.state.disabled} onClick={() => this.props.onLogin(this.state)}>Login</button></div>
         </div>
     }
 }
