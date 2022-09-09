ab7c499a1a664e038db309623ed06da9a661cd35
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-09 15:52:51 +0200

    function-components-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component {
-    render() {
-        return <div className="welcome">
-            <h2>Hello, {this.props.name}!</h2>
-            {this.props.age > 18 
-            ? <Age age={this.props.age}/>
-            : <p>You are very young!</p>}
-            </div>
-    }
+export function Welcome(props) {
+    return (
+        <div>
+            {props.name && <h2>Hello, {props.name}!</h2>}
+            {!props.name && <h2>Hello, stranger!</h2>}
+        </div>
+    )
 }
 
-Welcome.defaultProps = {
-    name: 'Stranger'
-  };
+
+
+// export class Welcome extends React.Component {
+//     render() {
+//         return <div className="welcome">
+//             <h2>Hello, {this.props.name}!</h2>
+//             {this.props.age > 18 
+//             ? <Age age={this.props.age}/>
+//             : <p>You are very young!</p>}
+//             </div>
+//     }
+// }
+
+// Welcome.defaultProps = {
+//     name: 'Stranger'
+//   };
