3fd58f80805adae5a3532b24161a5cb81a7e8d6e
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-14 12:14:29 +0200

    useEffect-01

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ClickCounter({ initialValue = 0 }) {
     const [counter, setCounter] = useState(initialValue)
+
+    const onCounterChange = () => {
+        console.log(`The counter is now ${counter}`)
+    }
+
+    useEffect(onCounterChange, [counter])
 
     function handleCounterIncrement() {
         setCounter(c => c + 1)
