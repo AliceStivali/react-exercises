209398584b20cee04ac62a604e0f8740ee4294a9
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-15 13:25:42 +0200

    custom-hooks-01

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,20 +1,34 @@
-import { useEffect, useState } from "react";
+import { useCounter } from "./useCounter"
 
-export function ClickCounter({ initialValue = 0 }) {
-    const [counter, setCounter] = useState(initialValue)
-
-    const onCounterChange = () => {
-        console.log(`The counter is now ${counter}`)
-    }
-
-    useEffect(onCounterChange, [counter])
-
-    function handleCounterIncrement() {
-        setCounter(c => c + 1)
-    }
+export function ClickCounter ({initialValue = 0}){
+    const { counter, onIncrement, onDecrement, onReset} = useCounter(initialValue)
 
     return <div>
-        <h2>Counter: {counter}</h2>
-        <button onClick={handleCounterIncrement}>Increment</button>
+        <h1>Count: {counter}</h1>
+        <button onClick={onIncrement}>Increment</button>
+        <button onClick={onDecrement}>Decrement</button>
+        <button onClick={onReset}>Reset</button>
     </div>
 }
+
+
+
+
+// export function ClickCounter({ initialValue = 0 }) {
+//     const [counter, setCounter] = useState(initialValue)
+
+//     const onCounterChange = () => {
+//         console.log(`The counter is now ${counter}`)
+//     }
+
+//     useEffect(onCounterChange, [counter])
+
+//     function handleCounterIncrement() {
+//         setCounter(c => c + 1)
+//     }
+
+//     return <div>
+//         <h2>Counter: {counter}</h2>
+//         <button onClick={handleCounterIncrement}>Increment</button>
+//     </div>
+// }
