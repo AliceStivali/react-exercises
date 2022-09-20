32db6c1243e7e2f356aa413899929e4f567a9dd6
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-20 17:02:31 +0200

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,60 +1,77 @@
-import React from "react";
-import { ClickTracker } from "./ClickTracker";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { TodoList } from "./TodoList";
-import { Container } from "./Container";
-import { LanguageContext } from "./LanguageContext";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { Sum } from "./Sum";
-import { ClickCounter } from "./ClickCounter";
-import { GithubUser } from "./GithubUser";
-import { GithubUserList } from "./GithubUserList";
-import { FilteredList } from "./FilteredList";
-import { CarDetails } from "./CarDetails";
+import { Routes, Route } from "react-router-dom";
+import { Welcome } from "./Welcome"
 
-
-
-export class App extends React.Component {
-    state = {
-        language: 'en'
-    }
-
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-
-    render() {
-        return <div><LanguageContext.Provider value={this.state.language}>
-            <select value={this.state.language} onChange={this.handleLanguageChange}>
-                <option value="en">ENGLISH</option>
-                <option value="it">ITALIANO</option>
-           </select>
-           <DisplayLanguage />
-                <Container title="My App">
-                    <InteractiveWelcome />
-                    <ClickCounter />
-                    {/* <Counter initialValue={0} incrementedBy={1} timeout={1000}/> */}
-                    <ClickTracker />
-                    <Login />
-                    <UncontrolledLogin />
-                    <TodoList 
-                    render={(state, remove) => state.todos.map((todo, index) => 
-                        <li key={index}>{todo} <button value={index} onClick={remove}>Remove</button></li>
-                        )
-                    }
-                    ></TodoList>
-                    <Sum />
-                    {/* <Counter initialValue={0}/> */}
-                    <GithubUser username='AliceStivali'/>
-                    <GithubUserList />
-                    <FilteredList />
-                    <CarDetails />
-                </Container>
-                   </LanguageContext.Provider>
-            </div>
-    }
+export function App() {
+    return(
+        <Routes>
+            <Route path="/" element={<Welcome name="Alice" />} />
+        </Routes>
+    )
 }
+
+
+
+
+
+
+
+// import React from "react";
+// import { ClickTracker } from "./ClickTracker";
+// import { InteractiveWelcome } from "./InteractiveWelcome";
+// import { Login } from "./Login";
+// import { UncontrolledLogin } from "./UncontrolledLogin";
+// import { TodoList } from "./TodoList";
+// import { Container } from "./Container";
+// import { LanguageContext } from "./LanguageContext";
+// import { DisplayLanguage } from "./DisplayLanguage";
+// import { Sum } from "./Sum";
+// import { ClickCounter } from "./ClickCounter";
+// import { GithubUser } from "./GithubUser";
+// import { GithubUserList } from "./GithubUserList";
+// import { FilteredList } from "./FilteredList";
+// import { CarDetails } from "./CarDetails";
+
+
+
+// export class App extends React.Component {
+//     state = {
+//         language: 'en'
+//     }
+
+//     handleLanguageChange = (event) => {
+//         this.setState({
+//             language: event.target.value
+//         })
+//     }
+
+//     render() {
+//         return <div><LanguageContext.Provider value={this.state.language}>
+//             <select value={this.state.language} onChange={this.handleLanguageChange}>
+//                 <option value="en">ENGLISH</option>
+//                 <option value="it">ITALIANO</option>
+//            </select>
+//            <DisplayLanguage />
+//                 <Container title="My App">
+//                     <InteractiveWelcome />
+//                     <ClickCounter />
+//                     {/* <Counter initialValue={0} incrementedBy={1} timeout={1000}/> */}
+//                     <ClickTracker />
+//                     <Login />
+//                     <UncontrolledLogin />
+//                     <TodoList 
+//                     render={(state, remove) => state.todos.map((todo, index) => 
+//                         <li key={index}>{todo} <button value={index} onClick={remove}>Remove</button></li>
+//                         )
+//                     }
+//                     ></TodoList>
+//                     <Sum />
+//                     {/* <Counter initialValue={0}/> */}
+//                     <GithubUser username='AliceStivali'/>
+//                     <GithubUserList />
+//                     <FilteredList />
+//                     <CarDetails />
+//                 </Container>
+//                    </LanguageContext.Provider>
+//             </div>
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom";
-import { App } from "./App";
 import './index.css'
+import { Root } from "./Root";
 
-const appElement = <App />
 const root = document.querySelector('#root')
 
-ReactDOM.render(appElement, root)
+ReactDOM.render(<Root />, root)
