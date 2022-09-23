65f8b61e097d00a1e8330b39ac0842bf6280ea23
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-09-23 16:02:12 +0200

    swr-01

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,30 +1,44 @@
-import { useState, useEffect } from "react"
+import useSWR from "swr"
+
+const fetcher = url => fetch(url).then(response => response.json())
+
 
 export function useGithubUser(username) {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
 
-    async function fetchFromGithub(username){
-        setLoading(true)
-        setError(null)
-
-        try {
-            const response = await fetch(`https://api.github.com/users/${username}`)
-            const json =  await response.json()
-
-            setData(json)
-        } catch (error) {
-            setError(error)
-            setData(null)
-        } finally {
-            setLoading(false)
-        }
+    return {
+        data: data,
+        error: error,
+        loading: !data && !error
     }
-    
-    useEffect(() => {
-        fetchFromGithub(username)
-    }, [username])
-
-    return { data, loading, error }
 }
+
+
+// export function useGithubUser(username) {
+//     const [data, setData] = useState(null)
+//     const [loading, setLoading] = useState(false)
+//     const [error, setError] = useState(null)
+
+//     async function fetchFromGithub(username){
+//         setLoading(true)
+//         setError(null)
+
+//         try {
+//             const response = await fetch(`https://api.github.com/users/${username}`)
+//             const json =  await response.json()
+
+//             setData(json)
+//         } catch (error) {
+//             setError(error)
+//             setData(null)
+//         } finally {
+//             setLoading(false)
+//         }
+//     }
+    
+//     useEffect(() => {
+//         fetchFromGithub(username)
+//     }, [username])
+
+//     return { data, loading, error }
+// }
