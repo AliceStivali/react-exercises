c19ce8ff985ec89c88452c29d93c892e5ce1172c
Alice Stivali <alicestivali@MacBook-Pro-di-Alice.local>
2022-12-12 17:25:11 +0100

    redux-01

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Root } from "./Root";
-import { store } from "./state/Store";
-import {
-  incrementCounter,
-  decrementCounter,
-  resetCounter,
-} from "./CounterState";
 
 const root = document.querySelector("#root");
 
-// ReactDOM.render(<Root />, root)
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(incrementCounter());
-store.dispatch(decrementCounter());
-store.dispatch(resetCounter());
+ReactDOM.render(<Root />, root);
